8c546e7d74996d9d1ef15589f509e58e46055562
Andrew Nissen <andrew_nissen@yahoo.com>
2013-08-29 10:12:44 -0400

    removed autohr for linebreaks in the middle

--- client/lib/medium.js
+++ client/lib/medium.js
@@ -345,7 +345,41 @@
                         cache.focusedElement = toFocus;
                         utils.cursor.set( 0, toFocus );
                     }
+                },
+                addLineBreak: function() {
+                    var sel, range;
+                    var html = "<br>";
                     
+                    if (window.getSelection) {
+                        // IE9 and non-IE
+                        sel = window.getSelection();
+                        if (sel.getRangeAt && sel.rangeCount) {
+                            range = sel.getRangeAt(0);
+                            range.deleteContents();
+
+                            // Range.createContextualFragment() would be useful here but is
+                            // non-standard and not supported in all browsers (IE9, for one)
+                            var el = document.createElement("div");
+                            el.innerHTML = html;
+                            var frag = document.createDocumentFragment(), node, lastNode;
+                            while ( (node = el.firstChild) ) {
+                                lastNode = frag.appendChild(node);
+                            }
+                            range.insertNode(frag);
+                            
+                            // Preserve the selection
+                            if (lastNode) {
+                                range = range.cloneRange();
+                                range.setStartAfter(lastNode);
+                                range.collapse(true);
+                                sel.removeAllRanges();
+                                sel.addRange(range);
+                            }
+                        }
+                    } else if (document.selection && document.selection.type != "Control") {
+                        // IE < 9
+                        document.selection.createRange().pasteHTML(html);
+                    }
                 }
             },
             
@@ -482,7 +516,7 @@
                         }
                         utils.html.addTag(settings.tags.paragraph, true, null, focusedElement);
                     } else {
-                        utils.html.addTag(settings.tags.paragraph, true, null, focusedElement);
+                        utils.html.addLineBreak();
                     }
                 }
             }

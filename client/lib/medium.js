c21eeb9283d933681079edfb7e67024cfc6e97c0
Andrew Nissen <andrew_nissen@yahoo.com>
2013-09-09 19:21:53 -0400

    fixed paragraph deletion bug

--- client/lib/medium.js
+++ client/lib/medium.js
@@ -254,7 +254,7 @@
                         innerText = utils.html.text(settings.element);
                     
                     // Empty Editer
-                    if( innerText === ""  ){
+                    if( (innerText === "") || innerText === null) ){
                         settings.element.innerHTML = '';
                         
                         // We need to add placeholders
